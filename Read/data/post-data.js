788462c8e593dba8ff0518699a62eef01f02344d
happykey <31617391+happykey@users.noreply.github.com>
2017-09-05 00:19:57 +0800

    lists&details

---
+++ Read/data/post-data.js
@@ -0,0 +1,55 @@
+var local_database = [
+  {
+    date: "Nov 18 2017",
+    title: "日出",
+    imgSrc: "../../images/sun-rise.jpg",
+    avatar: "../../images/avatar/1.png",
+    content: "誓水之滨，孑然独立。旭日东升，朝阳如墨。",
+    reading: "112",
+    collection: "96"
+  }, {
+    date: "July 20 2017",
+    title: "相依",
+    imgSrc: "../../images/visit.jpg",
+    avatar: "../../images/avatar/2.png",
+    content: "伊人相伴，泉水潺潺。",
+    reading: "521",
+    collection: "1111"
+  }, {
+    date: "July 20 2017",
+    title: "月圆",
+    imgSrc: "../../images/3.jpg",
+    avatar: "../../images/avatar/3.png",
+    content: "举剑邀月，斯人已矣。",
+    reading: "100",
+    collection: "30"
+  }, {
+    date: "June 11 2017",
+    title: "公主抱",
+    imgSrc: "../../images/4.jpg",
+    avatar: "../../images/avatar/4.png",
+    content: "怀抱乾坤。",
+    reading: "80",
+    collection: "130"
+  }, {
+    date: "June 11 2017",
+    title: "流光",
+    imgSrc: "../../images/5.jpg",
+    avatar: "../../images/avatar/5.png",
+    content: "公子持剑，沙场，点兵",
+    reading: "180",
+    collection: "230"
+  }, {
+    date: "June 11 2017",
+    title: "正阳",
+    imgSrc: "../../images/6.jpg",
+    avatar: "../../images/avatar/1.png",
+    content: "整装，负剑，阳光正好。",
+    reading: "880",
+    collection: "30"
+  }
+]
+
+module.exports={
+  postList: local_database
+}
